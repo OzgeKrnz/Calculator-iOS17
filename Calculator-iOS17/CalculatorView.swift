01f8640c44c82ef58cc56ba016416ebb08d72394
Özge Kurnaz <ozge.k60@hotmail.com>
2025-02-07 21:48:57 +0300

    Seperated View and Model layers

---
+++ Calculator-iOS17/CalculatorView.swift
@@ -0,0 +1,120 @@
+//
+//  CalculatorView.swift
+//  Calculator-iOS17
+//
+//  Created by özge kurnaz on 7.02.2025.
+//
+
+import UIKit
+class CalculatorView: UIView{
+    var stackView = UIStackView()
+    var rowStack = UIStackView()
+    
+    let displayLabel: UILabel = {
+        
+        let label = UILabel()
+        label.text = "0"
+        label.font = UIFont.systemFont(ofSize: 50, weight: .light)
+        label.textAlignment = .right
+        label.textColor = .white
+        label.numberOfLines = 1
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    let buttons = ["AC","+/-","%","÷",
+                   "7","8","9","×","4",
+                   "5","6","-","1","2",
+                   "3","+","0",".","="]
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        backgroundColor = .black
+        addSubview(displayLabel)
+        
+        // Display View Auto Layout
+        displayLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            displayLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            displayLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            displayLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            displayLabel.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        setupButtons()
+    }
+    
+    private func setupButtons(){
+        
+        //Stack View
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        
+        addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            stackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            stackView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5)
+        ])
+        
+        // satırları ayarlama
+        rowStack.axis = .horizontal
+        rowStack.spacing = 20
+        rowStack.distribution = .fillEqually
+        
+        for (index ,buttonTitle) in buttons.enumerated() {
+            let button = createButton(title: buttonTitle)
+            rowStack.addArrangedSubview(button)
+            
+            if (index + 1) % 4 == 0 {
+                stackView.addArrangedSubview(rowStack)
+                rowStack = UIStackView()
+                rowStack.axis = .horizontal
+                rowStack.spacing = 10
+                rowStack.distribution = .fillEqually
+            }
+         
+        }
+        if !rowStack.arrangedSubviews.isEmpty {
+            stackView.addArrangedSubview(rowStack)
+        }
+ 
+    }
+    
+    func createButton(title: String)->UIButton{
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 40)
+        button.setTitleColor(.white, for: .normal)
+        
+        let buttonColor: UIColor
+           if ["÷", "×", "-", "+", "="].contains(title) {
+               buttonColor = .orange
+           } else if ["AC", "+/-", "%"].contains(title) {
+               buttonColor = .lightGray
+           } else {
+               buttonColor = .darkGray
+           }
+        button.backgroundColor = buttonColor
+        button.layer.cornerRadius = 30
+        
+        return button
+        
+    }
+    
+ 
+    
+
+}
--- Calculator-iOS17/ViewController.swift
+++ Calculator-iOS17/ViewController.swift
@@ -9,82 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let calculatorView = CalculatorView()
     
-    let buttons = ["AC","+/-","%","÷","7","8","9","x","4","5","6","-","1","2","3","+","0",".","="]
+    override func loadView() {
+        view = calculatorView
+    }
     
-   
-    
-    
-    let displayLabel: UILabel = {
-        
-        let label = UILabel()
-        label.text = "0"
-        label.font = UIFont.systemFont(ofSize: 50, weight: .light)
-        label.textAlignment = .right
-        label.textColor = .white
-        label.numberOfLines = 1
-        label.adjustsFontSizeToFitWidth = true
-        return label
-    }()
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .black
-        
-        view.addSubview(displayLabel)
-        // Display View Auto Layout
-        displayLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            displayLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            displayLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            displayLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            displayLabel.heightAnchor.constraint(equalToConstant: 100)
-        ])
-        
-        let zeroBtn = createButton(title: "0", action: #selector(numberPressed(_ :)))
-        
-        view.addSubview(zeroBtn)
-        // Button Auto Layout
-        zeroBtn.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            zeroBtn.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
-            zeroBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            //btn genişlik ve yükseklik
-            zeroBtn.widthAnchor.constraint(equalToConstant: 80),
-            zeroBtn.heightAnchor.constraint(equalToConstant: 80)
-        ])
-        
-        
-        
-        
         
     }
     
-    @objc func numberPressed(_ sender: UIButton){
-        if let number = sender.currentTitle{
-            print("Basıldı: \(number)")
-        }
-    }
-    
-    func createButton(title: String, action:Selector)->UIButton{
-        let button = UIButton(type: .system)
-        button.backgroundColor = .darkGray
-        button.setTitle(title, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 40)
-        button.setTitleColor(.white, for: .normal)
-        button.layer.cornerRadius = 30
-        
-        //objc fonk cagırmak için
-        button.addTarget(self, action: action, for: .touchUpInside)
-        return button
-        
-    }
-    
-    
-   
-
 
 }
 
